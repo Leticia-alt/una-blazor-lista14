8f4f111936b267a65590e88932a0f75847632e73
Leticia-alt <petrinaleticia@gmail.com>
2026-04-24 20:18:52 -0300

    Add files via upload

---
+++ MeuCurriculo/Model/Experiencia.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MeuCurriculo.Model
+{
+    public class Experiencia
+    {
+        public string Empresa { get; set; }
+        public string Cargo { get; set; }
+
+        public string Inicio { get; set; } = "Atual"; 
+
+        public string Fim { get; set; }
+
+        public string Descricao { get; set; }
+    }
+}
